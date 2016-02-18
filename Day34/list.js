a3471bf435ba109b7b7a93a015dd2ec2460083df
mattycao <mattycao@gmail.com>
2016-02-18 11:55:52 +0800

    practice about list structure

---
+++ Day34/list.js
@@ -0,0 +1,104 @@
+// list type
+function list() {
+    this.listSize = 0;
+    this.pos = 0;
+    this.dataStore = [];
+    this.clear = clear;
+    this.find = find;
+    this.toString = toString;
+    this.insert = insert;
+    this.append = append;
+    this.remove = remove;
+    this.front = front;
+    this.end = end;
+    this.prev = prev;
+    this.next = next;
+    this.length = length;
+    this.currPos = currPos;
+    this.moveTo = moveTo;
+    this.getElement = getElement;
+    this.contains = contains;
+}
+
+function append(element) {
+    this.dataStore[this.listSize++] = element;
+}
+
+function find(element) {
+    for (var i = 0; i < this.dataStore.length; i++) {
+        if (this.dataStore[i] === element) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function remove(element) {
+    var index = this.find(element);
+    if (index >= 0) {
+        this.dataStore.splice(index, 1);
+        this.listSize--;
+        return true;
+    }
+    return false;
+}
+
+function length() {
+    return this.listSize;
+}
+
+function toString() {
+    return this.dataStore;
+}
+
+function insert(element, after) {
+    var index = this.find(after);
+    if (index > -1) {
+        this.dataStore.splice(index, 0, element);
+        this.listSize++;
+        return true;
+    }
+    return false;
+}
+
+function clear() {
+    delete this.dataStore;
+    this.dataStore = [];
+    this.listSize = this.pos = 0;
+}
+
+function contains(element) {
+    return (this.find(element) > -1);
+}
+
+function front() {
+    this.pos = 0;
+}
+
+function end() {
+    this.pos = this.listSize - 1;
+}
+
+function prev() {
+    if (this.pos > 0) {
+        this.pos--;
+    }
+}
+
+function next() {
+    if (this.pos < this.listSize - 1) {
+        this.pos++;
+    }
+}
+
+function currPos() {
+    return this.pos;
+}
+
+function moveTo(position) {
+    this.pos = position;
+}
+
+function getElement() {
+    return this.dataStore[this.pos];
+}
