4138a2001666a212d8fe3e167d7aac269077d606
mattycao <mattycao@gmail.com>
2016-03-09 10:32:56 +0800

    practice about ajax with node, refact the code by divided the functions

--- Day35/index.js
+++ Day35/index.js
@@ -1,63 +1,10 @@
 $(function () {
-    var octopus = {
-        init: function () {
-            ajax.init();
-            view.init();
-        }
-    };
-    var view = {
-        // here are all the event adding process
-        init: function () {
-            var regBtn = $('#regBtn');
-            regBtn.click(function (e) {
-                var user = {
-                    username: $('#formUserName').val(),
-                    age: $('#formUserAge').val()
-                };
-                ajax.post('/register', JSON.stringify(user));
-            });
-            var searchBtn = $('#searchBtn');
-            searchBtn.click(function (e) {
-                var username = $('#formUserName').val();
-                ajax.get('/search' + '?username=' + username);
-            });
-            var updateBtn = $('#updateBtn');
-            updateBtn.click(function (e) {
-                var user = {
-                    username: $('#formUserName').val(),
-                    age: $('#formUserAge').val()
-                };
-                ajax.post('/update', JSON.stringify(user));
-            });
-            var deleteBtn = $('#deleteBtn');
-            deleteBtn.click(function (e) {
-                var username = $('#formUserName').val();
-                ajax.get('/delete' + '?username=' + username);
-            });
-            var path = window.location.pathname;
-            if (path === '/' || path === '/spa.html') {
-                ajax.get('/all');
-            }
-        },
-        renderTable: function(users) {
-            var $table = $('table.table tbody');
-            var htmlStr = '';
-            users.forEach(function(user, index) {
-                htmlStr += '<tr>'+
-                        '<th scope="row">' + (++index)  + '</th>' +
-                        '<td>'+ user.username + '</td>' +
-                        '<td>'+ user.age + '</td>' +
-                        '</tr>';
-            });
-            $table.html(htmlStr);
-        }
-    };
-
+    // controller
     var ajax = {
         init: function () {
             this.xhr = new XMLHttpRequest();
         },
-        get: function (url) {
+        get: function (url, deBool) {
             var xhr = this.xhr;
             xhr.open('get', url, true);
             xhr.responseType = 'json';
@@ -65,18 +12,13 @@ $(function () {
                 if (xhr.readyState == 4) {
                     if (xhr.status == 200) {
                         var user = xhr.response;
-                        console.log(user);
                         if (url === '/all') {
                             view.renderTable(user);
+                        } else if (deBool) {
+                            view.renderDelete(user, url);
                         } else {
-                            var box = document.querySelector('.info');
-                            if (user) {
-                                box.innerHTML = JSON.stringify(user);
-                            } else {
-                                box.innerHTML = 'empty';
-                            }
+                            view.renderSearch(user, url);
                         }
-
                     }
                 }
             };
@@ -90,20 +32,159 @@ $(function () {
                 if (xhr.readyState == 4) {
                     if (xhr.status == 200) {
                         var user = xhr.response;
-                        var box = document.querySelector('.info');
-                        console.log(user);
-                        if (user) {
-                            box.innerHTML = user.username + ',' + user.age;
+                        var oUser = JSON.parse(data);
+                        if (url === '/register') {
+                            view.renderRegister(user, oUser);
                         } else {
-                            box.innerHTML = '';
+                            view.renderUpdate(user, oUser);
                         }
                     } else {
+                        view.renderError('Danger: The server response is bad, please contact the server admin.');
                     }
+                } else {
+                    view.renderError('Danger: The server is off, please contact the server admin.');
                 }
             };
             xhr.send(data);
         }
     };
 
-    octopus.init();
+    // starter
+    var starter = {
+        init: function () {
+            ajax.init();
+            view.init();
+        }
+    };
+
+    // view
+    var view = {
+        // here are all the event adding process
+        init: function () {
+            var regBtn = $('#regBtn');
+            var searchBtn = $('#searchBtn');
+            var updateBtn = $('#updateBtn');
+            var deleteBtn = $('#deleteBtn');
+            var path = window.location.pathname;
+            this.$table = $('table.table tbody');
+            this.$alertDiv = $('div.alert');
+            this.$formName = $('div.formName');
+            this.timer = null;
+            // event binding for the registration button
+            regBtn.click(function (e) {
+                var user = {
+                    username: $('#formUserName').val(),
+                    age: $('#formUserAge').val()
+                };
+                ajax.post('/register', JSON.stringify(user));
+            });
+            // event binding for the search button
+            searchBtn.click(function (e) {
+                var username = $('#formUserName').val();
+                ajax.get('/search' + '?username=' + username);
+            });
+            // event binding for the update button
+            updateBtn.click(function (e) {
+                var user = {
+                    username: $('#formUserName').val(),
+                    age: $('#formUserAge').val()
+                };
+                ajax.post('/update', JSON.stringify(user));
+            });
+            // event binding for the delete button
+            deleteBtn.click(function (e) {
+                var username = $('#formUserName').val();
+                ajax.get('/delete' + '?username=' + username, true);
+            });
+            // view route for the / and /spa/html
+            if (path === '/' || path === '/spa.html') {
+                ajax.get('/all');
+            }
+        },
+        _renderInit: function () {
+            this.$alertDiv.removeClass('sr-only').removeClass('alert-success').removeClass('alert-warning').removeClass('alert-danger');
+            this.$formName.removeClass('has-error');
+            if (this.timer) {
+                clearTimeout(this.timer);
+                this.timer = null;
+            }
+            this.timer = setTimeout(function () {
+                $('div.alert').addClass('sr-only');
+                $('div.formName').removeClass('has-error');
+            }, 5000);
+        },
+        renderTable: function (users) {
+            var htmlStr = '';
+            users.forEach(function (user, index) {
+                htmlStr += '<tr>' +
+                    '<th scope="row">' + (++index) + '</th>' +
+                    '<td>' + user.username + '</td>' +
+                    '<td>' + user.age + '</td>' +
+                    '</tr>';
+            });
+            this.$table.html(htmlStr);
+        },
+        renderSearch: function (user, url) {
+            var oUsername = url.replace('/search?username=', '');
+            this._renderInit();
+            var htmlStr = '';
+            if (user && user.age) {
+                this.$alertDiv.addClass('alert-success');
+                htmlStr = 'As shown in the server, ' + user.username + ' is now ' + user.age + ' years old.';
+            } else {
+                this.$alertDiv.addClass('alert-warning');
+                htmlStr = oUsername + ', whose information is not found in our server, please Try another name.';
+                this.$formName.addClass('has-error');
+            }
+            this.$alertDiv.html(htmlStr);
+        },
+        renderDelete: function (user, url) {
+            var oUsername = url.replace('/delete?username=', '');
+            this._renderInit();
+            var htmlStr = '';
+            if (user && user.age) {
+                this.$alertDiv.addClass('alert-success');
+                htmlStr = user.username + ' who is now ' + user.age + ' years old, has been deleted in the server.';
+            } else {
+                this.$alertDiv.addClass('alert-warning');
+                htmlStr = oUsername + ', whose information is not found in our server, please Try another name.';
+                this.$formName.addClass('has-error');
+            }
+            this.$alertDiv.html(htmlStr);
+        },
+        renderRegister: function (user, oUser) {
+            var htmlStr = '';
+            this._renderInit();
+            if (user && user.username) {
+                this.$alertDiv.addClass('alert-success');
+                htmlStr = user.username + ', who is ' + user.age + ' years old, has been added to the server.';
+            } else {
+                this.$alertDiv.addClass('alert-warning');
+                htmlStr = oUser.username + ', who is ' + oUser.age + ' years old, is has been uploaded in the server, please Try again.';
+                this.$formName.addClass('has-error');
+            }
+            this.$alertDiv.html(htmlStr);
+        },
+        renderUpdate: function (user, oUser) {
+            var htmlStr = '';
+            this._renderInit();
+            if (user) {
+                this.$alertDiv.addClass('alert-success');
+                htmlStr = user[0].username + ', who is originally recorded with ' + user[0].age + ' years old, has been changed to ' + user[1].age + ' the server.';
+            } else {
+                this.$alertDiv.addClass('alert-warning');
+                htmlStr = oUser.username + ', who is ' + oUser.age + ' years old, is not recorded in the server, please Try a user who has been recorded in our server.';
+                this.$formName.addClass('has-error');
+            }
+            this.$alertDiv.html(htmlStr);
+        },
+        renderError: function (str) {
+            this._renderInit();
+            this.$alertDiv.addClass('alert-danger');
+            this.$alertDiv.html(str);
+        }
+    };
+
+    // start the project
+    starter.init();
 });
--- Day35/server.js
+++ Day35/server.js
@@ -1,28 +1,17 @@
 /**
  * Created by caoyangkaka on 3/8/16.
  */
-//导入核心模块http
 var http = require('http');
-/**
- * 1. 能在特定的IP 特定端口上监听 客户端的请求
- * 2. 当请求到来的时候能执行监听函数，并返回响应
- *
- * 创建一个服务器
- * 指定监听函数 每当有客户端请求到来的时候执行的函数
- * request 代表客户端的请求，可以从中获取请求过来的信息
- * response 代表向客户端发的响应，可以通过它向客户端发响应
- *
- */
 var fs = require('fs');
 var mime = require('mime');
 var path = require('path');
-//node亲生的模块，帮助我们解析请求中的URL的
 var url = require('url');
 var users = [];
 var server = http.createServer(function (request, response) {
-    //把url转成url对象
     var urlObj = url.parse(request.url, true);
-
+    // for loging
+    console.log('LOG:' + request.method + '; ' + request.url + '; ' + 'TIME: ' + new Date().toString());
+    // all about router
     if (urlObj.pathname == '/') {
         response.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
         fs.readFile('./spa.html', function (err, data) {
@@ -35,81 +24,87 @@ var server = http.createServer(function (request, response) {
         });
         request.on('end', function () {
             var cUser = JSON.parse(str);
-            if(users.every(function(user) {
-                return user.username !== cUser.username;
-            })) {
+            if (users.every(function (user) {
+                    return user.username !== cUser.username || (user.username === cUser.username && user.age !== cUser.age);
+                })) {
                 users.push(cUser);
+                response.end(str);
+            } else {
+                response.end();
             }
-            (JSON.parse(str));
-            //最后返回用户列表
-            response.end(str);
         })
     } else if (urlObj.pathname == '/search') {
         var name = urlObj.query.username;
-        users.forEach(function (user) {
-            if (user.username === name) {
-                response.end(JSON.stringify(user));
-            }
+        var temp = users.find(function (user) {
+            return user.username === name;
         });
+        if (temp) {
+            response.end(JSON.stringify(temp));
+        } else {
+            response.end();
+        }
+    } else if (urlObj.pathname == '/update') {
+        var str = '';
+        request.on('data', function (data) {
+            str += data.toString();
+        });
+        request.on('end', function () {
+            var cUser = JSON.parse(str);
+            var temp = users.find(function (user) {
+                return user.username === cUser.username;
+            });
+            if (temp) {
+                var toUsers = [];
+                toUsers.push({
+                    username: temp.username,
+                    age: temp.age
+                });
+                temp.age = cUser.age;
+                toUsers.push(cUser);
+                response.end(JSON.stringify(toUsers));
+            } else {
+                response.end();
+            }
+        })
+    } else if (urlObj.pathname == '/delete') {
+        var name = urlObj.query.username;
+        for (var i = 0; i < users.length; i++) {
+            if (users[i].username === name) {
+                response.end(JSON.stringify(users[i]));
+                users.splice(i, 1);
+                return;
+            }
+        }
         response.end();
     } else if (urlObj.pathname == '/update') {
         var str = '';
         request.on('data', function (data) {
             str += data.toString();
         });
-        //当所有的数据全部接收完毕的时候会会触发end事件，这时请求体的数据就接收完整了
-        request.on('end', function () {
-            console.log(str);
-            //转成对象追加到用户列表里
-            var cUser = JSON.parse(str);
-            users.forEach(function (user) {
-                if (user.username === cUser.username) {
-                    user.age = cUser.age;
-                    response.end(JSON.stringify(user));
-                }
-            });
-            //最后返回用户列表
-            response.end('No change');
-        })
-    } else if (urlObj.pathname == '/delete') {
-        var name = urlObj.query.username;
-        users.forEach(function (user) {
-            if (user.username === name) {
-                users.splice(users.indexOf(user), 1);
-                console.log(user);
-                console.log(users);
-                response.end(JSON.stringify(user));
-            }
-        });
-        response.end();
-    } else if(urlObj.pathname == '/all') {
+    } else if (urlObj.pathname == '/all') {
         response.end(JSON.stringify(users));
     } else {
         var pathname = urlObj.pathname;
-        response.setHeader('Content-Type',mime.lookup(pathname)+';charset=utf-8');
-        fs.exists('.'+pathname,function(exists){
-            if(exists){
-                fs.readFile('.'+pathname,function(err,data){
-                    //如果读取文件出错了，则也报404错误
-                    if(err){
+        response.setHeader('Content-Type', mime.lookup(pathname) + ';charset=utf-8');
+        response.setHeader('Cache-Control', 'max-age=2592000');
+        fs.exists('.' + pathname, function (exists) {
+            if (exists) {
+                fs.readFile('.' + pathname, function (err, data) {
+                    if (err) {
                         response.statusCode = 404;
                         response.end();
-                    }else{
+                    } else {
                         response.statusCode = 200;
                         response.write(data);
                         response.end();
                     }
 
                 })
-            }else{
+            } else {
                 response.statusCode = 404;
                 response.end();
             }
 
         })
     }
-});
-//在8080端口上进行监听 ，主机名是localhost
-// 0 - 65535
-// ps -ef | grep node
-server.listen(8080, 'localhost');
+}).listen(8080, 'localhost');
